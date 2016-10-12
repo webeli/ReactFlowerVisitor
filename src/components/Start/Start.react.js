78716f987aa4669b3f4f2df663b07b1d7cb9a625
earthquaker <pe_lias@msn.com>
2016-10-12 15:25:30 +0200

    style and UI changes

--- src/components/Start/Start.react.js
+++ src/components/Start/Start.react.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 import Geosuggest from 'react-geosuggest';
 import '../../styles/geosuggest.css';
+import '../../styles/Start.css';
+
+import { Navbar, Nav, NavItem, Col, Well, FormGroup, ControlLabel, Button, FormControl } from 'react-bootstrap';
 
 class Start extends Component {
 
@@ -19,12 +22,55 @@ class Start extends Component {
 
         return (
             <div>
-                <h2>Start</h2>
-                <Geosuggest
-                    placeholder="Skicka till adress"
-                    fixtures={fixtures}
-                    country="se"
-                    onSuggestSelect={this.onSuggestSelect}/>
+                <Navbar>
+                    <Navbar.Header>
+                        <Navbar.Brand>
+                            <a href="#">BLOOMDO</a>
+                        </Navbar.Brand>
+                        <Navbar.Toggle />
+                    </Navbar.Header>
+                    <Navbar.Collapse>
+                        <Nav>
+                            <NavItem eventKey={1} href="#">Kontakta oss</NavItem>
+                        </Nav>
+                        <Nav pullRight>
+                            <NavItem eventKey={2} href="#">Nyheter</NavItem>
+                            <NavItem eventKey={2} href="#">Hjälp</NavItem>
+                            <NavItem eventKey={1} href="#">Logga in (Florister)</NavItem>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Navbar>
+                <Col xs={12} md={4} mdOffset={3}>
+                    <h2 className="center-text">Kreativa florister i närheten</h2>
+                    <Well>
+                        <h3 className="center-text">Ange leveransadress</h3>
+                        <Geosuggest
+                            placeholder="Skicka till adress"
+                            fixtures={fixtures}
+                            country="se"
+                            onSuggestSelect={this.onSuggestSelect}/>
+                    </Well>
+                </Col>
+                <Col xs={12} md={2}>
+                    <h2 className="center-text">Florist? Joina oss!</h2>
+                    <Well>
+                        <form>
+                            <FormGroup controlId="formValidationSuccess1">
+                                <ControlLabel>Bolagsnamn</ControlLabel>
+                                <FormControl type="text" />
+                            </FormGroup>
+                            <FormGroup controlId="formValidationSuccess1">
+                                <ControlLabel>Telefonnummer</ControlLabel>
+                                <FormControl type="text" />
+                            </FormGroup>
+
+                            <Button type="submit">
+                                Gå vidare
+                            </Button>
+                        </form>
+                    </Well>
+                </Col>
+
             </div>
         );
     }
